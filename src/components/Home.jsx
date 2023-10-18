ad55cec1db8b9550f6f980fc769240d32a5dedba
Rajan4405 <rajanshatma652@gmail.com>
2023-10-18 15:58:03 +0545

    Add image and Home content

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,13 @@
+import { Box, Image, Text } from '@chakra-ui/react'
 import React from 'react'
+import btc from "../assets/btc.png"
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+  return <Box bgColor={"blackAlpha.900"} w={"full"} h={"85vh"}>
+    <Image w={"full"} h={"full"} objectFit={"contain"} src={btc} filter={"grayscale(1)"} />
+    <Text fontSize={"6xl"} textAlign={"center"} fontWeight={"thin"} color={"whiteAlpha.700"} mt={"-20"}>Crypto World</Text>
+
+  </Box>
 }
 
 export default Home
